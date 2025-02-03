aef28a8c6eb91cc0230d91c380baf0af3f427e2d
Ikkyu <126569245+wattanunteeratanapong@users.noreply.github.com>
2025-02-03 21:25:36 +0700

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using BasicASP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicASP.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/SignupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicASP.Controllers
+{
+    public class SignupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
