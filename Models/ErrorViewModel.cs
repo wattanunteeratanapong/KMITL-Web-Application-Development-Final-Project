7ae9c85f2a661e0bafc903115ff2ffa0de51c3d1
Ikkyu <126569245+wattanunteeratanapong@users.noreply.github.com>
2025-02-08 15:25:43 +0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BasicASP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
